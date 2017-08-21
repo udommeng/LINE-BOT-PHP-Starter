207ed124d69d901c72815309a86e469bb60d5459
Udommeng.PNC <udommeng@gmail.com>
2017-08-21 15:17:37 +0700

    aa

--- senddata_.php
+++ senddata_.php
@@ -20,7 +20,7 @@ $messages = [
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/push';
 $data = [
-    'to' => $userID1,'to' => $userID2,
+    'to' => $userID1,
     'messages' => [$messages],
 ];
 
@@ -36,4 +36,21 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
+
+$data = [
+    'to' => $userID2,
+    'messages' => [$messages],
+];
+
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' .$access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
 echo $post;
