86fe0dbf5db322d95f5d323c33c8c0feb1af4a0e
Udommeng.PNC <udommeng@gmail.com>
2017-08-23 21:54:05 +0700

    aaa

--- TestConnectMysql.php
+++ TestConnectMysql.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-</head>
-<body>
-
-    <h1>ทดสอบ เชื่อมต่อฐานข้อมูล กับ azure</h1>
-
-<?
+<?php
 
 	ini_set('display_errors', 1);
 	error_reporting(~0);
@@ -39,9 +26,6 @@
 
 	mysqli_close($conn);
 ?>
-
-</body>
-</html>
 
 
 
