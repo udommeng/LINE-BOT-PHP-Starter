0615772702a2cf742b7e06e6b7022269ee020689
Udommeng.PNC <udommeng@gmail.com>
2017-08-23 11:05:01 +0700

    ss

--- TestConnectMysql.php
+++ TestConnectMysql.php
@@ -1,9 +1,16 @@
-<html>
+<<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>ThaiCreate.Com PHP & MySQL (mysqli)</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
 </head>
 <body>
-    test
-
+    <h1>test</h1>
 </body>
 </html>
+
+
+
