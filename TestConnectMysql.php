2f04f139ac14beaaaf460b5e172260e4e513d631
Udommeng.PNC <udommeng@gmail.com>
2017-08-23 21:45:19 +0700

    ฟฟฟ

--- TestConnectMysql.php
+++ TestConnectMysql.php
@@ -26,7 +26,7 @@
    $userPassword = "U_e2qx15";
    $dbName = "misschoo_line";
   
-	$conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+	$conn = mysqli_connect($serverName,$userName,$userPassword,$dbName,"3306");
 
 	if (mysqli_connect_errno())
 	{
