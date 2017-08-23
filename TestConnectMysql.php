d54321f27c25a0c16da1f29a099d097a0a7f796e
Udommeng.PNC <udommeng@gmail.com>
2017-08-23 11:30:28 +0700

    a

--- TestConnectMysql.php
+++ TestConnectMysql.php
@@ -16,12 +16,12 @@
 	ini_set('display_errors', 1);
 	error_reporting(~0);
 
-   $serverName = "ap-cdbr-azure-southeast-b.cloudapp.net";
-   $userName = "b7d6df679dcdd2";
-   $userPassword = "4f6be5a0";
-   $dbName = "misschool";
+   $serverName = "mysql-5.5.chaiyohosting.com";
+   $userName = "misschoo_line";
+   $userPassword = "U_e2qx15";
+   $dbName = "misschoo_line";
   
-	$conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+	$conn = mysqli_connect($serverName,$userName,$userPassword);
 
 	if (mysqli_connect_errno())
 	{
