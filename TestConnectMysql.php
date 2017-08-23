4393083665020e110e6b3fbfcff1ae8f6da64e6d
Udommeng.PNC <udommeng@gmail.com>
2017-08-23 11:10:32 +0700

    test5

--- TestConnectMysql.php
+++ TestConnectMysql.php
@@ -9,8 +9,36 @@
 </head>
 <body>
     <h1>test</h1>
+
+<?
+
+	ini_set('display_errors', 1);
+	error_reporting(~0);
+
+   $serverName = "localhost";
+   $userName = "root";
+   $userPassword = "root";
+   $dbName = "mydatabase";
+  
+	$conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+
+	if (mysqli_connect_errno())
+	{
+		echo "Database Connect Failed : " . mysqli_connect_error();
+	}
+	else
+	{
+		echo "Database Connected.";
+	}
+
+	mysqli_close($conn);
+?>
+
 </body>
 </html>
+
+
+
 
 
 
