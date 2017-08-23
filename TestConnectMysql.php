b051983c7b618f3c484bb580980076335d8ec9c3
Udommeng.PNC <udommeng@gmail.com>
2017-08-23 23:02:26 +0700

    test

--- TestConnectMysql.php
+++ TestConnectMysql.php
@@ -2,15 +2,12 @@
 
 
 	//insertUserId("ทดสอบ","Test");
-
+	function insert_data_line($userId1,$message1){
 	$serverName = "ap-cdbr-azure-southeast-b.cloudapp.net";
 	$userName = "b7d6df679dcdd2";
 	$userPassword = "4f6be5a0";
 	$dbName = "misschool";
 	
-	$userId1 = "test";
-	$message1 = "ทดสอบ";
-
 	$conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 
 	$sql = "insert into tbl_line(userId,message) values('.$userId1','.$message1')";
@@ -24,6 +21,10 @@
 	}
 
 	mysqli_close($conn);
+
+}
+
+	insert_data_line("test1","ทดสอบ1");
 ?>
 
 
