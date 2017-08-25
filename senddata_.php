72ac25f4582eb5d0dc74e98ba1dc62661beef093
Udommeng.PNC <udommeng@gmail.com>
2017-08-25 10:35:15 +0700

    test

--- senddata_.php
+++ senddata_.php
@@ -5,7 +5,7 @@ $access_token = 'q30MmQc3ML9v7/o1R4yqt+HSDZvoupL04oSpI3LXl/nE43Vh49M0v4B69oFRIJj
 
 //-- user id ที่จะทำการส่ง --
 $userID1 = 'U3ecb2860486ae1014519e0d957ae51c4';
-$userID2 = 'U7a60698bcec0bc20dacf1c8be8775b24';
+$userID2 = 'U02196f392b0efbedefbfe0996d77ff34';
 
 $date = date("Y-m-d");
 $time = date("H:i:s");
